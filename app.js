34ee51243531b666e159197c7d11c1ba0d5b442e
geesaladurgaakhil <56544604+geesaladurgaakhil@users.noreply.github.com>
2026-04-09 07:51:02 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const http = require('http');
 
 const server = http.createServer((req, res)=> {
-   res.end('Hello from cloud Run 23');
+   res.end('Hello from cloud Run 24');
 })
 server.listen(8080);
